7a27fb424a6613089e366c854e946caa56f17e0b
beniel05 <benielebenezer@gmail.com>
2026-06-01 18:18:36 +0530

    Practice 1. Reverse an array using functional Recursion, 2. To check the given string is palindrome or not

--- basicRecursion/BasicRecursion2.java
+++ basicRecursion/BasicRecursion2.java
@@ -8,6 +8,10 @@ class BasicRecursion2 {
 
         /* 2. Factorial of a Number using Recursion */
         
+        /* 3. Reverse an array only using one pointer and recursion */
+        
+        /* 4. Check if the given String is Palindrome or not using functional recursion */
+
         int n = 3;
 
         // 1
@@ -17,6 +21,16 @@ class BasicRecursion2 {
         // 2
         // System.out.println(factorialOfN(n));
 
+        // 3
+        // int[] arr = {10, 30, 60, 40, 50};
+        // System.out.println(Arrays.toString(arr));
+        // reverseArr(0, arr, arr.length);
+
+        // 4
+        String str = "Madam";
+        boolean res = checkTheStringIsPalindromeOrNot(0, str);
+        if(res) System.out.println(str + " is Palindrome");
+        else System.out.println(str + " is NOT Palindrome");
     }
 
     static void sumOf_N_Natural_Numbers_Parameter_Printing(int i, int s) {
@@ -35,6 +49,37 @@ class BasicRecursion2 {
     static int factorialOfN(int n) {
         if(n < 1) return 1;
         return n * factorialOfN(n - 1);
+    }
+
+    static void reverseArr(int i, int[] a, int n) {
+
+        if(i >= n / 2) {
+            System.out.println(Arrays.toString(a));
+            return;
+        }
+
+        System.out.println("Before Swap: " + a[i] + " " + a[n - i - 1]);
+        
+        int temp = a[i];
+        a[i] = a[n - i - 1];
+        a[n - i - 1] = temp;
+        
+        System.out.println("After Swap: " + a[i] + " " + a[n - i - 1]);
+
+        reverseArr(i + 1, a, n);
+
+    }
+
+    static boolean checkTheStringIsPalindromeOrNot(int i, String s) {
+        
+        if(i >= s.length() / 2) return true;
+
+        char left = Character.toLowerCase(s.charAt(i));
+        char right = Character.toLowerCase(s.charAt(s.length() - i - 1));
+
+        if(left != right) return false;
+
+        return checkTheStringIsPalindromeOrNot(i + 1, s);
     }
 
 }
