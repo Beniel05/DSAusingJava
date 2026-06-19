e27144bb0b531bb01d6a262cf183a293236ead85
beniel05 <benielebenezer@gmail.com>
2026-06-19 15:24:39 +0530

    Revisited SecondLargest problem and then Practiced check the given array is sorted or not

--- array/SecondLargest.java
+++ array/SecondLargest.java
@@ -3,10 +3,8 @@ import java.util.Arrays;
 public class SecondLargest {
     // Second distinct largest element;
 
-    public static void main(String[] args) {
-
-        int[] arr = { 10, 20, 40, 30, 50, 70, 50, 80, 80 };
-
+    static void findSecondLargest(int[] arr) {
+        // OPTIMIZED SOLUTION;
         int largest = Integer.MIN_VALUE;
         int secondLargest = largest;
 
@@ -14,14 +12,57 @@ public class SecondLargest {
             if (arr[i] > largest) {
                 secondLargest = largest;
                 largest = arr[i];
-            } else if (arr[i] > secondLargest && arr[i] != largest) {
+            } else if (arr[i] < largest && arr[i] > secondLargest) {
                 secondLargest = arr[i];
             }
         }
 
         System.out.println("Array: " + Arrays.toString(arr));
         System.out.println("Largest: " + largest);
-        System.out.println("Second Largest: " + secondLargest);
+
+        if (secondLargest != Integer.MIN_VALUE) {
+            System.out.println("Second Largest: " + secondLargest);
+        } else {
+            System.out.println("THERE IS NO DISTINCT SECOND LARGEST ELEMENT IN THE ARRAY");
+        }
+
+        System.out.println(); // empty line after getting the output
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 10, 20, 40, 30, 50, 70, 50, 80, 80 };
+
+        // OPTIMIZED SOLUTION; O(N) -> single for loop
+        findSecondLargest(arr);
+
+        int[] arr2 = { 80, 80, 80 };
+        findSecondLargest(arr2);
+
+        // BETTER SOLUTION;
+        // using 2 different for loops finding both largest and second largest
+        // separately.
+        // O(2N) -> 2 different no nested for loops.
+
+        // BRUTE FORCE;
+        // Sort the array and the largest = arr[arr.length - 1] (last element)
+        // traverse to find the second largest from the end to start.
+        // O(N log N) for sorting and O(N) for traversing.
+
+        /*
+         * Arrays.sort(arr);
+         * int l = arr[arr.length - 1];
+         * int sl = arr[0];
+         * for(int i = arr.length - 2; i >= 0; i--) {
+         * if(arr[i] != l) {
+         * sl = arr[i];
+         * break;
+         * }
+         * }
+         * System.out.println(l);
+         * System.out.println(sl);
+         */
+
     }
 
 }
---
+++ array/SortedOrNot.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class SortedOrNot {
+
+    static void findTheArrayIsSortedOrNot(int[] arr) {
+
+        boolean isSorted = true;
+        for(int i = 1; i < arr.length; i++) {
+
+            if(arr[i - 1] > arr[i]) {
+                isSorted = false;
+                break;
+            }
+        }
+
+        if(isSorted) {
+            System.out.println("The given array: " + Arrays.toString(arr) + " is sorted!");
+        } else {
+            System.out.println("The given array: " + Arrays.toString(arr) + " is NOT sorted.");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        
+        int[] arr1 = {10, 20, 30, 30, 40, 50};
+
+        int[] arr2 = {10, 20, 30, 40, 50, 30};
+        Arrays.sort(arr2);
+
+        int[] arr3 = {10, 20, 20, 30, 20, 40};
+
+        findTheArrayIsSortedOrNot(arr1);
+        findTheArrayIsSortedOrNot(arr2);
+        findTheArrayIsSortedOrNot(arr3);
+
+    }
+}
